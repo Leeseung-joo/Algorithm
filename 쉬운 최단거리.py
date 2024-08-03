7062fd9067c6e1520fc5e8b046256e977d66586b
Lee seungjoo <eun07213@gmail.com>
2024-08-04 00:15:53 +0900

    대표적인 bfs문제

--- 쉬운 최단거리.py
+++ 쉬운 최단거리.py
@@ -1,19 +1,38 @@
-
+import sys
+input = sys.stdin.readline
+from collections import deque
 n,m = map(int,input().split())
-matrix = [list(map(int,input().split())) for _ in range(n)]
+graph = [list(map(int,input().split())) for _ in range(n)]
+visited = [[-1]*m for _ in range(n)]
+dx,dy = [0,0,-1,1], [-1,1,0,0]
+
+def bfs(i,j):
+    queue = deque()
+    queue.append((i,j))
+    
+    visited[i][j] = 0
+    
+    while queue:
+        x,y = queue.popleft()
+        
+        for i in range(4):
+            nx,ny = x + dx[i], y+dy[i]
+            
+            if 0<=nx<n and 0 <=ny <m and visited[nx][ny] == -1:
+                if graph[nx][ny] == 0: 
+                    visited[nx][ny] = 0
+                elif graph[nx][ny] == 1:
+                    visited[nx][ny] = visited[x][y] + 1
+                    queue.append((nx,ny))
 for i in range(n):
     for j in range(m):
-        if matrix[i][j] == 2:
-            a,b = i,j
-for i in range(n):
-    for j in range(m):
-        if matrix[i][j] == 1:
-            matrix[i][j] = abs(i-a+j-b)
-        elif matrix[i][j] == 2:
-            matrix[i][j] = 0
+        if graph[i][j] == 2 and visited[i][j] == -1:
+            bfs(i,j)
+for i in range(N):
+    for j in range(M):
+        if graph[i][j] == 0:
+            print(0, end=' ')
         else:
-            continue
-for i in range(m):
-    for j in range(n):
-        print(matrix[i][j], end = ' ')
-    print(sep = '\n')
+            print(visited[i][j], end=' ')
+    print()
+            
