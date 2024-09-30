efbdf40be7cc8133f8322cd1b8d7c42ce7028f83
Lee seungjoo <eun07213@gmail.com>
2024-09-30 22:03:33 +0900

    수정

--- 연속합.py
+++ 연속합.py
@@ -1,6 +1,6 @@
 n = int(input())
-list1 = list(map(int,input))
-dp = [0] * n
+list1 = list(map(int,input().split()))
+dp = [0] * (n)
 dp[0] = list1[0]
 for i in range(1,n):
     dp[i] = max(list1[i], list1[i]+dp[i-1]) #현재꺼 혹은 이전 최대 연속합 + 현재
